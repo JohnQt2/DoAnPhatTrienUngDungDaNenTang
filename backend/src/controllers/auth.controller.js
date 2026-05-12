2095df7afc4fa931b27dfd1d9901ab66dfd06dc1
pminh4869 <kimtaehyungbwiiiii@gmail.com>
2026-05-12 23:20:22 +0700

    Xử lý register / login / me

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+import { sendSuccess } from '../utils/response.js';
+import { normalizeRegisterPayload, normalizeLoginPayload } from '../utils/validators.js';
+import { registerUser, loginUser } from '../services/auth.service.js';
+
+export const register = async (request, response) => {
+  const payload = normalizeRegisterPayload(request.body);
+  const result = await registerUser(payload);
+  sendSuccess(response, result, 201);
+};
+
+export const login = async (request, response) => {
+  const payload = normalizeLoginPayload(request.body);
+  sendSuccess(response, await loginUser(payload));
+};
+
+export const me = async (request, response) => {
+  sendSuccess(response, { user: request.user });
+};
